18645aee9a8ae6291e3534265929e036e73a7c4a
GabrielFelix23 <srgfelix123@gmail.com>
2020-08-24 16:42:34 -0300

    Montando o cabeçalho.

--- App.js
+++ App.js
@@ -8,6 +8,15 @@ import {useFonts, Anton_400Regular} from '@expo-google-fonts/anton'
 import Routes from './src/router'
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Anton_400Regular
+  })
+
+  if(!fontsLoaded){
+    return<AppLoading/>
+  }
+  
   return (
     < >
       <StatusBar style="light" backgroundColor="#000" translucent={true} />
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,64 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { MaterialIcons} from '@expo/vector-icons'
 
 export default function Home() {
   return (
-    <View>
-        <Text>Pagina Home</Text>
+    <View style={styles.container}>
+        <View style={styles.header}>
+            <Image
+                source={require("../../assets/banner.png")}
+                style={styles.image}
+            />
+
+            <View style={styles.textContainer}>
+                <Text style={styles.text}>TÊNIS</Text>
+                <Text style={[styles.text, {color: '#cececf'}]}>.</Text>
+                <Text style={[styles.text, {color: '#cececf'}]}>MASCULINO</Text>
+
+                <TouchableOpacity style={{position: 'absolute', right: 0, alignSelf: 'center'}}>
+                    <MaterialIcons
+                        name="filter-list"
+                        size={24}
+                        color="#000"
+                    />
+                </TouchableOpacity>
+            </View>
+        </View>
+
+        <View style={styles.line}/>
+
+        <ScrollView>
+            <Text style={styles.text}>LANÇAMENTO</Text>
+        </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        width: '100%',
+        backgroundColor: "#fff"
+    },
+    header:{
+        marginBottom: 8
+    },
+    image:{
+        width: '100%'
+    },
+    textContainer:{
+        flexDirection: 'row',
+        marginVertical: '5%',
+        marginHorizontal: '5%'
+    },
+    text:{
+        fontFamily: 'Anton_400Regular',
+        fontSize: 26,
+        marginHorizontal: '1%'
+    },
+    line:{
+        borderBottomColor: "#d8d8d8",
+        borderBottomWidth: 2
+    }
+})
